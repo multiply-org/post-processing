94c0633d7962bc6a0ae60f6e0eeea530ee3e0895
Tonio Fincke <tonio.fincke@brockmann-consult.de>
2017-04-06 16:16:22 +0200

    set up dummies

---
+++ dummy-post-processing-application/dummy_post_processing/__init__.py
@@ -0,0 +1,1 @@
+from dummy_post_processor import dummy_post_processor
---
+++ dummy-post-processing-application/dummy_post_processing/dummy_post_processor.py
@@ -0,0 +1,4 @@
+class dummy_post_processor:
+
+    def post_process(self, biophysical_params):
+        print ('Performing post-processing operations on the biophysical parameters')
