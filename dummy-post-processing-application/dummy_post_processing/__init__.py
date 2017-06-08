4f4f811fbc6b0d008083addc44dfc8aab262c96d
Tonio Fincke <tonio.fincke@brockmann-consult.de>
2017-06-08 21:06:29 +0200

    removed unnecessary files

--- dummy-post-processing-application/dummy_post_processing/__init__.py
+++
@@ -1,1 +0,0 @@
-from dummy_post_processor import dummy_post_processor
--- dummy-post-processing-application/dummy_post_processing/dummy_post_processor.py
+++
@@ -1,4 +0,0 @@
-class dummy_post_processor:
-
-    def post_process(self, biophysical_params):
-        print ('Performing post-processing operations on the biophysical parameters')
