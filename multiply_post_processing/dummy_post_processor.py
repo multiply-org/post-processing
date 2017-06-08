88a6741966e3db83c6c1e40185f17b96b153ce1f
Tonio Fincke <tonio.fincke@brockmann-consult.de>
2017-06-08 20:48:19 +0200

    use conda

---
+++ multiply_post_processing/dummy_post_processor.py
@@ -0,0 +1,4 @@
+class DummyPostProcessor:
+
+    def post_process(self, biophysical_params):
+        print ('Performing post-processing operations on the biophysical parameters')
---
+++ setup.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python
+
+from setuptools import setup
+
+setup(name='multiply-post-processing',
+      version='0.1',
+      description='MULTIPLY Post Processing',
+      author='MULTIPLY Team',
+      packages=['multiply_post_processing'],
+     )
