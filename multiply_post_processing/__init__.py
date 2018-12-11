450b66bedc6892472be0841eeae45d84700dfe87
TonioF <tonio.fincke@brockmann-consult.de>
2018-12-11 11:22:43 +0100

    edited init

--- multiply_post_processing/__init__.py
+++ multiply_post_processing/__init__.py
@@ -1,4 +1,5 @@
-from .post_processing import add_post_processor_creator, get_post_processor_names
 from .post_processor import EODataPostProcessor, IndicatorDescription, PostProcessor, VariablePostProcessor, \
     PostProcessorCreator, PostProcessorType
+from .burned_severity_post_processor import BurnedSeverityPostProcessorCreator
+from .post_processing import add_post_processor_creator, get_post_processor_names
 from .version import __version__
