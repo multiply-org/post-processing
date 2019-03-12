fc9fc046952dbcfce6a4c34361322753df01f5ec
Tonio Fincke <tonio.fincke@brockmann-consult.de>
2019-03-12 17:37:20 +0100

    added application information

--- test/post_processing/indicators/test_indicators.py
+++ test/post_processing/indicators/test_indicators.py
@@ -7,11 +7,13 @@ __author__ = 'Tonio Fincke (Brockmann Consult GmbH)'
 def test_get_indicators():
     indicators = get_indicators()
 
-    assert 1 == len(indicators)
+    assert 5 == len(indicators)
     assert 'geocbi' == indicators[0]['Variable']['short_name']
     assert 'Geo CBI' == indicators[0]['Variable']['display_name']
     assert indicators[0]['Variable']['unit'] is None
     assert '0-3' == indicators[0]['Variable']['range']
+    assert 1 == len(indicators[0]['Variable']['applications'])
+    assert 'fire severity' in indicators[0]['Variable']['applications']
 
 
 def test_indicators_are_in_variables():
