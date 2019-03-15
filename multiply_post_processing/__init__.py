9a03187709c9d6a63bff67444604c08df1706cb9
Tonio Fincke <tonio.fincke@brockmann-consult.de>
2019-03-15 17:50:30 +0100

    added export

--- multiply_post_processing/__init__.py
+++ multiply_post_processing/__init__.py
@@ -2,6 +2,6 @@ from .post_processor import EODataPostProcessor, PostProcessor, PostProcessorCre
     VariablePostProcessor
 from .burned_severity_post_processor import BurnedSeverityPostProcessorCreator
 from .functional_diversity_metrics_post_processor import FunctionalDiversityMetricsPostProcessorCreator
-from .post_processing import add_post_processor_creator, get_available_indicators, get_post_processor_names, \
-    run_post_processing, run_post_processor
+from .post_processing import add_post_processor_creator, get_available_indicators, get_post_processor_description,\
+    get_post_processor_names, run_post_processing, run_post_processor
 from .version import __version__
