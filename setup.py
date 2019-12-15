cd0d6b3af9908cca56208bf8abce58c7019c6578
TonioF <tonio.fincke@brockmann-consult.de>
2019-12-15 19:49:12 +0100

    removed variables from setup

--- setup.py
+++ setup.py
@@ -29,7 +29,7 @@ setup(name='multiply-post-processing',
               'functional_diversity_metrics_post_processor_creator = multiply_post_processing:'
               'functional_diversity_metrics_post_processor.FunctionalDiversityMetricsPostProcessorCreator'
           ],
-          'variables': ['indicators = multiply_post_processing.indicators:indicators.get_indicators']
+          # 'variables': ['indicators = multiply_post_processing.indicators:indicators.get_indicators']
       },
       install_requires=requirements
 )
